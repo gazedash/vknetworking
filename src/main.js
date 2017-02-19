a179f011b7351fa73d9b2f674bd3f9aecd3b9d44
shoegazerwithak <skrtroot@protonmail.com>
2017-02-19 15:04:24 +0300

    Add RxJS and Scroll

--- src/main.js
+++ src/main.js
@@ -1,11 +1,44 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-
+import App from './App';
+ import {fetchCommunities, fetchMembers} from './vk_api'
+ import Vue from 'vue'
+ import Rx from 'rxjs/Rx'
+import {isBottomOfPage} from "./utils/index";
+ const groupStream = Rx.Observable.fromPromise(fetchCommunities(126125228))
+   .flatMap(el => Rx.Observable.from(el))
+   .bufferCount(1);
+ let prom;
+ const scrollStream = Rx.Observable.fromEvent(window, 'scroll')
+   .filter(() => {
+     if (isBottomOfPage()) console.log("Bottom of page");
+     return isBottomOfPage();
+   }).throttleTime(500);
+ scrollStream.subscribe(el => console.log(el));
 /* eslint-disable no-new */
-new Vue({
+const app = new Vue({
   el: '#app',
-  template: '<App/>',
+  data: {
+    list: [],
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  template: '<App :list="list"/>',
   components: { App }
-})
+});
+
+const initial = groupStream.find(el => {
+  prom = Rx.Observable.fromPromise(fetchMembers(el, 139));
+  return prom.subscribe(
+    (members) => members.length,
+    () => false,
+  );
+}).map(() => {
+  return prom;
+});
+
+initial.subscribe(el => {
+  el.subscribe((da) => {
+    console.log(da);
+    app.list = app.list.concat(da)
+  });
+});
---
+++ src/vk_api/index.js
@@ -0,0 +1,45 @@
+
+//const api_key = "API_KEY";
+import fetch from 'isomorphic-fetch'
+const api_key = "API_KEY";
+const endpoint = "https://api.vk.com";
+//
+function baseMethod(method) {
+  return `${endpoint}/method/${method}?access_token=${api_key}`
+}
+
+export function buildGetMembersUrl(id, city = "", sex = 0, age_from = 0, age_to = 0) {
+  // sex=1: female
+  return baseMethod('users.search') + `&group_id=${id}&fields=photo_max,first_name,last_name&online=0` +
+    `${city != "" ? "&city=" + city : ""}&sex=${sex}` +
+    `${age_from ? "&age_from=" + age_from : ""}` +
+    `${age_to ? "&age_to=" + age_to : ""}` +
+    `&has_photo=1&count=1000`;
+  // hometown?
+}
+
+export function buildGetSubscriptions(userId) {
+  return baseMethod('users.getSubscriptions') + `&user_id=${userId}&extended=0`;
+}
+
+export function fetchCommunities(userId) {
+  return Promise.resolve(
+    JSON.parse("MOCK_DATA")
+  ).then(data => data);
+  // return fetch(buildGetSubscriptions(userId), {mode: 'cors'})
+    // .then(response => response.json())
+    // .then(json => json.response.groups.items);
+}
+
+export function fetchMembers(id, city = "", sex = 0, age_from = 0, age_to = 0) {
+  return fetch(buildGetMembersUrl(id, city, sex, age_from, age_to))
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      if (json.response) {
+        json.response.shift()
+      }
+      return json.response;
+    });
+}
