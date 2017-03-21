3700a0cb39519e9afa1f1c03e8b2c55987741726
shoegazerwithak <skrtroot@protonmail.com>
2017-03-21 20:22:05 +0300

    Lazy directive: use update hook (if url is changed)

--- src/directives/lazy.js
+++ src/directives/lazy.js
@@ -1,23 +1,32 @@
 import {isElementInViewport} from "../utils/index";
+
+function lazyLoad(el, img) {
+  if (isElementInViewport(el)) {
+    el.src = img.src;
+  } else {
+    setTimeout(() => {
+      el.src = img.src;
+    }, Math.floor(100 + (Math.random() * 300)))
+  }
+}
+
+function load(el, binding) {
+  let img = new Image();
+  img.src = binding.value;
+
+  img.onload = () => {
+    lazyLoad(el, img)
+  };
+  img.onerror = () => {
+    lazyLoad(el, img)
+  }
+}
+
 export default {
   bind (el, binding) {
-    function lazyLoad(el, img) {
-      if (isElementInViewport(el)) {
-        el.src = img.src;
-      } else {
-        setTimeout(() => {
-          el.src = img.src;
-        }, Math.floor(100 + (Math.random() * 300)))
-      }
-    }
-    let img = new Image();
-    img.src = binding.value;
-
-    img.onload = () => {
-      lazyLoad(el, img)
-    };
-    img.onerror = () => {
-      lazyLoad(el, img)
-    }
+    load(el, binding)
+  },
+  update (el, binding) {
+    load(el, binding)
   }
 };
