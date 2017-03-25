b6ecd2f19b7a2e5da0a024e1b0cc917d38fa5979
shoegazerwithak <skrtroot@protonmail.com>
2017-03-25 13:16:08 +0300

    Auth: add flow annotations & fix eslint errors

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,20 +1,22 @@
-export let user = {
+// @flow
+export const user = {
   authenticated: false,
+  firstTime: true,
   doNotShowAgain: false,
 };
 
-export function logout () {
+export function logout() {
   delete localStorage.token;
   user.authenticated = false;
 }
 
-export function redirected(to, from, next) {
+export function redirected(to: Object, from: Object, next: Function = () => {}) {
   if (to.hash) {
     const userId = to.hash.split('user_id=')[1];
-    localStorage.setItem("lastLoginUserId", userId);
-    const token = to.hash.split('access_token=')[1].split("&")[0];
+    localStorage.setItem('lastLoginUserId', userId);
+    const token = to.hash.split('access_token=')[1].split('&')[0];
     if (token) {
-      localStorage.setItem("token", token);
+      localStorage.setItem('token', token);
       user.authenticated = true;
     } else {
       window.close();
@@ -25,25 +27,31 @@ export function redirected(to, from, next) {
   next('/');
 }
 
-export function ifLoggedIn(to, from, next) {
+export function checkAuth() {
+  const token = localStorage.getItem('token');
+  user.authenticated = !!token;
+  return !!token;
+}
+
+export function ifLoggedIn(to: Object, from: Object, next: Function = () => {}) {
   console.log('to login');
   if (checkAuth()) {
-    next('/')
+    next('/');
   } else {
     next();
   }
 }
 
-export function requireAuth (to, from, next) {
+export function requireAuth(to: Object, from: Object, next: Function = () => {}) {
   if (!checkAuth()) {
     console.log(!checkAuth());
     next({
       path: '/login',
       // query: {redirect: to.fullPath}
-    })
+    });
   } else {
     console.log('else');
-    next()
+    next();
   }
 }
 
@@ -51,17 +59,16 @@ export function checkFirstTimeAndSet() {
   if (localStorage.firstTime) {
     user.firstTime = false;
     return false;
-  } else {
-    user.firstTime = true;
-    localStorage.firstTime = 'no';
-    return true;
   }
+  user.firstTime = true;
+  localStorage.firstTime = 'no';
+  return true;
 }
 
-export function checkAuth() {
-  let token = localStorage.getItem('token');
-  user.authenticated = !!token;
-  return !!token;
+export function checkShow() {
+  const doNotShowAgain = localStorage.getItem('doNotShowAgain');
+  user.doNotShowAgain = !!doNotShowAgain;
+  return doNotShowAgain;
 }
 
 export function initialCheck() {
@@ -70,14 +77,9 @@ export function initialCheck() {
   checkFirstTimeAndSet();
 }
 
-export function checkShow() {
-  let doNotShowAgain = localStorage.getItem('doNotShowAgain');
-  user.doNotShowAgain = !!doNotShowAgain;
-  return doNotShowAgain;
-}
-
 export function setDoNotShowAgain() {
-  let doNotShowAgain = localStorage.setItem('doNotShowAgain', true);
+  // const doNotShowAgain = localStorage.setItem('doNotShowAgain', true);
+  localStorage.setItem('doNotShowAgain', true);
   user.doNotShowAgain = true;
   return true;
 }
